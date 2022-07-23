6fcc7a43f6913348451702204be9dfbb6f197103
KABIR <kabir_karamatjanov@mail.ru>
2022-07-23 09:38:52 +0500

    movies-app project finihed

--- script.js
+++ script.js
@@ -1,69 +1,74 @@
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
 const form = document.getElementById("form");
-const input = document.getElementById("input");
-const todosUL = document.getElementById("todos");
+const search = document.getElementById("search");
 
-const todos = JSON.parse(localStorage.getItem("todos"));
+// initially get fav movies
+getMovies(APIURL);
 
-if (todos) {
-    todos.forEach((todo) => {
-        addTodo(todo);
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies(respData.results);
+}
+
+function showMovies(movies) {
+    // clear main
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
     });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    addTodo();
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
 });
-
-function addTodo(todo) {
-    let todoText = input.value;
-
-    if (todo) {
-        todoText = todo.text;
-    }
-
-    if (todoText) {
-        const todoEl = document.createElement("li");
-        if (todo && todo.completed) {
-            todoEl.classList.add("completed");
-        }
-
-        todoEl.innerText = todoText;
-
-        todoEl.addEventListener("click", () => {
-            todoEl.classList.toggle("completed");
-
-            updateLS();
-        });
-
-        todoEl.addEventListener("contextmenu", (e) => {
-            e.preventDefault();
-
-            todoEl.remove();
-
-            updateLS();
-        });
-
-        todosUL.appendChild(todoEl);
-
-        input.value = "";
-
-        updateLS();
-    }
-}
-
-function updateLS() {
-    const todosEl = document.querySelectorAll("li");
-
-    const todos = [];
-
-    todosEl.forEach((todoEl) => {
-        todos.push({
-            text: todoEl.innerText,
-            completed: todoEl.classList.contains("completed"),
-        });
-    });
-
-    localStorage.setItem("todos", JSON.stringify(todos));
-}
